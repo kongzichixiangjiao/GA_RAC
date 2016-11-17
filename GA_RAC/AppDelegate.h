6456b480d6f46d6dc5fa2fef3c1faa909be372ea
kongzichixiangjiao <58895347@qq.com>
2016-11-17 17:06:27 +0800

    Initial Commit

---
+++ GA_RAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GA_RAC
+//
+//  Created by houjianan on 2016/11/17.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
